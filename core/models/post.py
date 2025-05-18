38a8131763ad53086126ea78e7c91620de68c51b
kudiyarhan <romeo333_91@mail.ru>
2025-05-18 23:33:07 +0300

    create user relation mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,18 +1,18 @@
-from typing import TYPE_CHECKING
 
-from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy import String, Text
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from .mixins import UserRelationMixin
+from sqlalchemy.orm import Mapped, mapped_column
 
-if TYPE_CHECKING:
-    from .user import User
+
 
 # объявление модели для создания таблицы
-class Post(Base):
+class Post(UserRelationMixin, Base):
 
     # __tablename__ = 'products'
 
     # при помощи Mapped указываем что, это будут названия колонок таблицы
+    _user_back_populates = 'posts'
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
         Text,
@@ -20,11 +20,5 @@ class Post(Base):
         server_default="",
     )
 
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-    )
 
-# создаем связь многие ко многим с таблицей users для этого импортируем
-# модуль relationship. т,к нельзя импортировать класс USER, импортируем 
-# TYPE_CHECKING
-    user: Mapped['User'] = relationship(back_populates='posts')
+
